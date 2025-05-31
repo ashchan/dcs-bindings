158cbbcf79a1bf8b53584d83576b19295f4b6011
Ash <ashchan@gmail.com>
2025-05-31 09:50:00 +0900

    Check in F16 bindings

---
+++ F-16C_50/joystick/VKBsim Gunfighter TMW {D221ED90-2676-11ef-800C-444553540000}.diff.lua
@@ -0,0 +1,29 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3013pnilu3013cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+			["name"] = "Target Management Switch - Right",
+		},
+		["d3020pnilu3020cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "CAMERA/GUN Trigger - FIRST DETENT",
+		},
+		["d3021pnilu3021cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "CAMERA/GUN Trigger - SECOND DETENT",
+		},
+	},
+}
+return diff
